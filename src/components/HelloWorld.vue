71d9e3f8c09d388eba9e6c77267d2da439d378f1
puresite23 <mrkartseva@gmail.com>
2023-07-27 17:13:28 +0300

    change helloWorld page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,6 +22,7 @@ const count = ref(0)
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
+        CRM MOMENT
   </p>
   <p>
     Install
